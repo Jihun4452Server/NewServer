cb391fd6680022a904e758fb239f132fd70a3d29
박지훈 <jihun6548@naver.com>
2025-11-19 22:36:11 +0900

    feat: encoding 추가

--- src/main/java/org/pro/newserver/application/user/UserMapper.java
+++ src/main/java/org/pro/newserver/application/user/UserMapper.java
@@ -1,13 +1,17 @@
 package org.pro.newserver.application.user;
 
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.pro.newserver.application.user.dto.UserCommand;
 import org.pro.newserver.domain.user.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
 	@Mapping(target = "id", ignore = true)
-	User toUser(UserCommand command);
+	@Mapping(target = "password",
+		expression = "java(passwordEncoder.encode(command.password()))")
+	User toUser(UserCommand command, @Context PasswordEncoder passwordEncoder);
 }
